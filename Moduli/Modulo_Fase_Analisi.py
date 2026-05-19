e8b57395de4c6bddbb5eb7f07330e8f2e84087ca
Pietro Dossi <pietro.dossi@gmail.com>
2026-05-19 20:18:44 +0200

    fase 4 - messa a punto metriche e forecast

--- Moduli/Modulo_Fase_Analisi.py
+++ Moduli/Modulo_Fase_Analisi.py
@@ -29,3 +29,28 @@ import Moduli.Modulo_Configurazione as pd_config
 import Moduli.Modulo_Common as pd_common
 
 
+# CALCOLO INDICE DI PERFORMANCE
+# MAE - MEAN ABSOLUTE ERROR - ERRORE ASSOLUTO MEDIO
+# MAE_TEST
+def mae(y_reali, y_predetti):
+    return round(np.mean(np.abs(y_reali - y_predetti)), 2)
+
+
+
+def confronto_mae(target_supervisionato, target_previsionale, ambiente:str):
+    # MAE PREVISIONALE SU DATI DI TEST
+    MAE_TEST_SORG =  mae(target_supervisionato , np.mean(target_supervisionato))
+
+    # MAE PREVISIONALE SU DATI DI TRAINING
+    MAE_TEST_PREV =  mae(target_supervisionato, np.mean(target_previsionale))
+
+    print(f'MAE {ambiente} SORGENTE       : {MAE_TEST_SORG}')
+    print(f'MAE {ambiente} PREVISIONALE  : {MAE_TEST_PREV}')
+
+
+def arraytargetToDataframe(arrayTarget):
+    dfforecast_target = df = pd.DataFrame({
+        "row_num": range(len(arrayTarget)),
+        "target": arrayTarget
+    })
+    return dfforecast_target
